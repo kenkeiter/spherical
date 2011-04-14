9de94d3bb56542d34026e7b6cb205e41d7a34e17
kenkeiter <ken@kenkeiter.com>
2011-04-13 22:03:33 -0700

    Removed inventory function, since it's been replaced by the Spherical::Traversable module, and additions to the PropertyCollector

--- lib/spherical/types/folder.rb
+++ lib/spherical/types/folder.rb
@@ -28,6 +28,8 @@ module Spherical
     include Searchable # allow Folders to be searched
     include Traversable # this entity has children
     
+    # Create a new folder beneath this one. Return a ManagedReference to 
+    # the new folder.
     def create_subfolder(name)
       create_folder(:name => name)
     end
@@ -46,60 +48,6 @@ module Spherical
       else
         nil
       end
-    end
-    
-    def inventory(criteria = {})
-      property_spec = [{ :type => 'Folder', :pathSet => ['name', 'parent']}]
-      criteria.each do |ptype, options|
-        ptype = ptype.kind_of?(ManagedReference) ? ptype.type.to_s : ptype.to_s
-        criteria_spec = {:type => ptype, :all => (options == :all)}
-        (criteria_spec[:pathSet] = options << 'parent') unless options == :all
-        property_spec << criteria_spec
-      end
-      
-      results = @host.service.property_collector.retrieve_properties do |xml|
-        xml.vim25(:specSet, 'xsi:type' => 'PropertyFilterSpec'){
-          # specify properties to select for each object returned
-          property_spec.each do |prop_set|
-            xml.propSet{
-              xml.type prop_set[:type]
-              xml.all !!prop_set[:all]
-              prop_set[:pathSet].each{|path| xml.pathSet(path) } if prop_set[:pathSet]
-            }
-          end
-          # specify objects to filter
-          xml.objectSet{
-            @host.api.coerce_to_xml(xml, {:obj => self})
-            xml.skip false
-            xml.tag!(:selectSet, 'xsi:type' => 'TraversalSpec') {
-              xml.vim25(:name, 'visitFolders')
-              xml.vim25(:type, 'Folder')
-              xml.vim25(:path, 'childEntity')
-              xml.vim25(:skip, false)
-              xml.vim25(:selectSet, 'xsi:type' => 'SelectionSpec') {
-                xml.vim25(:name, 'visitFolders')
-              }
-            }
-          }
-        }
-      end
-      
-      # Convert the result to a tree.
-      structure = {}
-      obj_results = results.kind_of?(Array) ? results : [results]
-      obj_results.each do |obj_result|
-        prop_set = obj_result.prop_set.kind_of?(Array) ? obj_result.prop_set : [obj_result.prop_set._hash]
-        prop_set = Hash[prop_set.map{ |p| [p['name'], p['val']] }]
-        puts obj_result, prop_set
-        unless prop_set['parent']
-          structure[obj_result.obj.apply_attributes(prop_set)] = {}
-        else
-          structure[prop_set['parent']][prop_set['name']] = [obj_result.obj, prop_set]
-        end
-      end
-      
-      return structure
-      
     end
      
   end
